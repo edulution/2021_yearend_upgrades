dac8f4d96ecb02575323535d6a1740465a6c6feb
Kapya Sakala <kapsakala@gmail.com>
2021-12-17 22:33:28 +0200

    upgrade node version to 10.24 using n. reinstall dependencies for assessment tool

---
+++ scripts/09install_npm_pkgs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo npm -g install n --save
+
+# Install nodejs v10.24.1
+sudo n 10
+
+sudo npm -g install forever --save
+sudo npm -g install csv --save
+sudo npm -g install body-parser --save
+sudo npm -g install express --save
+sudo npm -g install pg --save
+sudo npm -g install knex --save
+sudo npm -g install sequelize --save
+sudo npm -g install axios --save
+sudo npm -g install vue --save
+sudo npm -g install webpack --save
