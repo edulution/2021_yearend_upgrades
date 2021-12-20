944b13e99229f50838702d560515480dc0dd8913
Kapya Sakala <kapsakala@gmail.com>
2021-12-20 15:25:13 +0200

    added script to clean up files

---
+++ scripts/013cleanup_files.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Clean up downloads directory
+cd ~/Downloads || exit
+
+rm -f -- *.csv
+rm -f -- *.R
+rm -f -- *.py
+rm -f -- *.whl
+rm -f -- *.sh
+rm -f -- *.zip
+rm -f -- *.gz
+rm -f -- *.backup
+rm -f -- *.png
+
+
+# Clean up home directory
+cd ~ || exit
+
+rm -f -- *.csv
+rm -f -- *.R
+rm -f -- *.py
+rm -f -- *.whl
+rm -f -- *.sh
+rm -f -- *.zip
+rm -f -- *.gz
+rm -f -- *.backup
+rm -f -- *.png
+
+rm -rf new*
+rm -rf content*
+
+
+# Clean up desktop
+cd ~/Desktop || exit
+rm -f -- *.csv
+rm -f -- *.R
+rm -f -- *.py
+rm -f -- *.whl
+rm -f -- *.sh
+rm -f -- *.zip
+rm -f -- *.gz
+rm -f -- *.backup
+rm -f -- *.png
+rm -rf new*
+rm -rf content*
+
+
+
+# Clean up backups directory
+cd ~/backups || exit
+
+# Remove any file that begins with an underscore. It is likely a currupt backup
+rm -f -- _*
+rm -f -- *.csv
+rm -f -- *.R
+rm -f -- *.py
+rm -f -- *.whl
+rm -f -- *.sh
+rm -f -- *.gz
+rm -f -- *.backup
